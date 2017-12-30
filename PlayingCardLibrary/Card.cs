b48d750fc89c47436c0545f25def679cc9a006ce
gothandy <gothandy@hotmail.com>
2017-12-30 06:12:12 +0000

    New Card passing.

--- PlayingCardLibrary/Card.cs
+++ PlayingCardLibrary/Card.cs
@@ -12,5 +12,10 @@ namespace PlayingCardLibrary
             this.rank = rank;
             this.suit = suit;
         }
+
+        public override string ToString()
+        {
+            return String.Format("{0} of {1}s", this.rank, this.suit);
+        }
     }
 }
