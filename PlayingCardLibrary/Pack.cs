862d78e0a75cb0bc257248f02f74fef1193c642e
gothandy <gothandy@hotmail.com>
2017-12-30 07:19:16 +0000

    New Pack tests passing.

--- PlayingCardLibrary/Pack.cs
+++ PlayingCardLibrary/Pack.cs
@@ -8,7 +8,16 @@ namespace PlayingCardLibrary
     {
         List<Card> cards = new List<Card>();
 
-
+        public Pack()
+        {
+            foreach(Rank rank in Enum.GetValues(typeof(Rank)))
+            {
+                foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+                {
+                    cards.Add(new Card(rank, suit));
+                }
+            }
+        }
 
         public int Count => cards.Count;
 
