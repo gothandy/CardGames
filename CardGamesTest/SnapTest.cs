3e8f0e380f722eb1675cc3f038f227cfc7fa7166
gothandy <gothandy@hotmail.com>
2017-12-30 20:31:27 +0000

    Added theory to count both players #pass

--- CardGamesTest/SnapTest.cs
+++ CardGamesTest/SnapTest.cs
@@ -6,14 +6,18 @@ namespace CardGamesTest
 {
     public class SnapTest
     {
-        [Fact]
-        public void NewGame()
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void NewGame(int player)
         {
             Pack pack = new Pack();
 
             SnapPlayers snapPlayers = new SnapPlayers(2);
 
             snapPlayers.Deal(pack, numberOfCards: 26);
+
+            Assert.Equal(26, snapPlayers[player].Hand.Count);
 
         }
     }
