6405cb37ea725633b6478ecacefeeb10b7b65351
gothandy <gothandy@hotmail.com>
2017-12-30 08:50:23 +0000

    Split out Card and New Pack tests #pass

---
+++ CardGamesTest/CardTests.cs
@@ -0,0 +1,34 @@
+﻿using PlayingCardLibrary;
+using System;
+using Xunit;
+
+namespace CardGamesTest
+{
+    public class CardTests
+    {
+        [Fact]
+        public void NewTest()
+        {
+            Card card = new Card(Rank.Ace, Suit.Spade);
+
+            Assert.Equal("Ace of Spades", card.ToString());
+        }
+
+        [Fact]
+        public void EqualsTest()
+        {
+            Card card1 = new Card(Rank.Ace, Suit.Spade);
+            Card card2 = new Card(Rank.Ace, Suit.Spade);
+
+            Assert.Equal<Card>(card1, card2);
+        }
+
+        [Fact]
+        public void HashTest()
+        {
+            Card card = new Card(Rank.Ace, Suit.Spade);
+
+            Assert.Equal<int>(1, card.GetHashCode());
+        }
+    }
+}
---
+++ CardGamesTest/NewPackTests.cs
@@ -0,0 +1,59 @@
+﻿using PlayingCardLibrary;
+using System;
+using Xunit;
+
+namespace CardGamesTest
+{
+    public class NewPackTests
+    {
+
+        [Fact]
+        public void Count()
+        {
+            Pack pack = new Pack();
+
+            Assert.Equal(52, pack.Count);
+        }
+
+        [Fact]
+        public void OrientationFaceDown()
+        {
+            Pack pack = new Pack();
+
+            Assert.Equal(Orientation.FaceDown, pack.Orientation);
+        }
+
+
+        [Fact]
+        public void BottomCard()
+        {
+            Pack pack = new Pack();
+
+            Assert.Equal<Card>(new Card(Rank.Ace, Suit.Spade), pack[0]);
+        }
+
+        [Fact]
+        public void TopCard()
+        {
+            Pack pack = new Pack();
+
+            Assert.Equal<Card>(new Card(Rank.King, Suit.Heart), pack[51]);
+        }
+
+        [Fact]
+        public void SecondBottomCard()
+        {
+            Pack pack = new Pack();
+
+            Assert.Equal<Card>(new Card(Rank.Two, Suit.Spade), pack[1]);
+        }
+
+        [Fact]
+        public void SecondTopCard()
+        {
+            Pack pack = new Pack();
+
+            Assert.Equal<Card>(new Card(Rank.Queen, Suit.Heart), pack[50]);
+        }
+    }
+}
