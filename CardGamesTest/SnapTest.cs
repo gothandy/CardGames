5536393bd60dd566cf8c636882347450ee6f7755
gothandy <gothandy@hotmail.com>
2017-12-30 20:39:40 +0000

    Flip Card #notimplemented #fail

--- CardGamesTest/SnapTest.cs
+++ CardGamesTest/SnapTest.cs
@@ -20,5 +20,19 @@ namespace CardGamesTest
             Assert.Equal(26, snapPlayers[player].Hand.Count);
 
         }
+
+        [Fact]
+        public void FlipCard()
+        {
+            Pack pack = new Pack();
+
+            SnapPlayers snapPlayers = new SnapPlayers(2);
+
+            snapPlayers.Deal(pack, numberOfCards: 26);
+
+            snapPlayers[0].FlipCard();
+
+            Assert.Equal<Card>(new Card(Rank.Ace, Suit.Spade), snapPlayers[0].FaceUpPile[0]);
+        }
     }
 }
--- PlayingCardLibrary/SnapPlayer.cs
+++ PlayingCardLibrary/SnapPlayer.cs
@@ -1,6 +1,14 @@
-﻿namespace PlayingCardLibrary
+﻿using System;
+
+namespace PlayingCardLibrary
 {
     public class SnapPlayer : Player
     {
+        public void FlipCard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pile FaceUpPile = new Pile(Orientation.FaceUp);
     }
 }
